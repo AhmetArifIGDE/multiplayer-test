9038d67c7feb0e8c6641d8f0e5289f7edcc76da2
AhmetArifIGDE <86314808+AhmetArifIGDE@users.noreply.github.com>
2024-04-27 22:11:35 +0300

    unity multiplayer eğitimi
    unity ve pun kullanarak 2v2 basic oyun geliştirme

---
+++ multiplayer bitti/Assets/oyuncu.cs
@@ -0,0 +1,139 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class oyuncu : MonoBehaviour
+{
+    PhotonView pw;
+    int saglik = 100;
+    public GameObject[] noktalar;
+    int CanSayisi;
+    int HedefOyuncu;
+    float rotationX;
+    float rotationY;
+
+    void Start()
+    {
+        CanSayisi = 10;
+        pw = GetComponent<PhotonView>();
+       
+        if (pw.IsMine)
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                transform.position = noktalar[0].transform.position;
+                HedefOyuncu = 1;
+            }
+            else
+            {
+                transform.position = noktalar[1].transform.position;
+                HedefOyuncu = 0;
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pw.IsMine)
+        {
+
+            // oyun bittiğinde oyuncuların kontrolünü durdur
+            // 2.oyuncu bağlanmadan ilk bağlanan hareket edemesin.
+            // biri odadan çıkarsa kalan oyuncu hareket edemesin ve istatistikler sıfırlansın
+            // İlk ekranda kullanıcı adına playerpref eklenerek, kullanıcı tekrar geldiğinde seçmiş olduğu
+            // ad oraya otomatik yazılsın yani tekrar tekrar isim seçmesin.
+            hareket();
+            zipla();
+            atesEt();
+             // transform.Rotate(Input.mousePosition.x, Input.mousePosition.y, 0 * 10f * Time.deltaTime);
+
+           
+
+              if (Input.GetAxis("Mouse X")<0)
+               {
+                   transform.Rotate((Vector3.up) * -1.5f);
+
+               }
+               if (Input.GetAxis("Mouse X") > 0)
+               {
+                   transform.Rotate((Vector3.up) * 1.5f);
+
+               }
+            
+           
+        }     
+
+         
+
+
+    }
+    void atesEt()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            RaycastHit hit;
+           
+            if (Physics.Raycast(transform.position,transform.forward, out hit,100f))
+            {
+
+                if (hit.transform.gameObject.CompareTag("Dusman"))
+                {
+                    //  hit.collider.gameObject.GetComponent<PhotonView>().RPC("darbever", RpcTarget.All, 20);
+                    hit.collider.gameObject.GetComponent<PhotonView>().RPC("CanAzalt", RpcTarget.All, HedefOyuncu);
+                }
+
+
+
+            }
+            
+
+        }
+
+    }
+
+    void hareket()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical") * 20f);
+        transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * 20f);      
+
+    }
+
+    void zipla()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 10, 0);
+
+        }
+        
+
+    }
+
+    [PunRPC]
+    void darbever(int darbegucu)
+    {
+        saglik -= darbegucu;
+        Debug.Log("Kalan Sağlık" + saglik);
+        if (saglik<=0)
+        {
+            PhotonNetwork.Destroy(gameObject);
+
+        }
+        
+    }
+
+    [PunRPC]
+    void CanAzalt(int HedefOyuncu)
+    {
+        CanSayisi--;
+       
+        GameObject.FindWithTag("SunucuYonetimi").GetComponent<sunucuyonetim>().SkorlariGuncelle(HedefOyuncu, CanSayisi);
+    }
+}
---
+++ multiplayer bitti/Assets/sunucuyonetim.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class sunucuyonetim : MonoBehaviourPunCallbacks
+{
+
+    
+    public Text serverbilgi;
+    public InputField kulad;
+    public InputField odaadi;
+    string Kullanıcıadi;
+    string OdaAdi;
+    
+
+
+    void Start()
+    {
+        
+        
+       PhotonNetwork.ConnectUsingSettings();
+
+        /*  if(PhotonNetwork.IsConnected)
+          {
+              serverbilgi.text = "Servere Bağlandı";
+
+          }*/
+
+        DontDestroyOnLoad(gameObject);
+       
+    }
+    public void OdaKur()
+    {
+        SceneManager.LoadScene(1);
+        Kullanıcıadi = kulad.text;
+        OdaAdi = odaadi.text;
+        PhotonNetwork.JoinLobby();     
+
+    }
+    public void GirisYap()
+    {
+        SceneManager.LoadScene(1);
+        Kullanıcıadi = kulad.text;
+        OdaAdi = odaadi.text;
+        PhotonNetwork.JoinLobby();    
+
+    }
+    public override void OnConnectedToMaster()
+    {
+        serverbilgi.text = "Servere Bağlandı";
+
+    }
+    public override void OnJoinedLobby()
+     {
+        if (Kullanıcıadi!="" && OdaAdi!="")
+        {
+            PhotonNetwork.JoinOrCreateRoom(OdaAdi, new RoomOptions { MaxPlayers = 2, IsOpen = true, IsVisible = true }, TypedLobby.Default);
+
+        }else
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        
+    }
+    public override void OnJoinedRoom() 
+    {
+        InvokeRepeating("BilgiKontrolEt", 0, 1f);
+        GameObject objem = PhotonNetwork.Instantiate("Oyuncu", Vector3.zero, Quaternion.identity, 0, null);        
+        objem.GetComponent<PhotonView>().Owner.NickName = Kullanıcıadi;
+        
+    }
+    public override void OnLeftRoom()
+    {
+        Debug.Log("Odadan çıkıldı");
+
+    } 
+    public override void OnLeftLobby()
+    {
+        Debug.Log("Lobiden çıkıldı");
+
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+      
+        InvokeRepeating("BilgiKontrolEt", 0, 1f);
+    }
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        // herhangi bir oyuncu girdiğinde tetiklenen fonksiyondur.
+    }
+
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Herhangi bir odaya girilemedi");
+
+    }
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Rastgele bir odaya girilemedi");
+
+    }
+    public override void OnCreateRoomFailed(short returnCode, string message)
+      {
+        Debug.Log("Oda oluşturulamadı");
+
+    }
+
+    void BilgiKontrolEt()
+    {
+
+        if (PhotonNetwork.PlayerList.Length == 2)
+        {
+            GameObject.FindWithTag("OyuncuBekleniyor").GetComponent<TextMeshProUGUI>().text = "";
+            GameObject.FindWithTag("Oyuncu_1").GetComponent<TextMeshProUGUI>().text = PhotonNetwork.PlayerList[0].NickName;
+            GameObject.FindWithTag("Oyuncu_2").GetComponent<TextMeshProUGUI>().text = PhotonNetwork.PlayerList[1].NickName;
+            CancelInvoke("BilgiKontrolEt");
+
+        }else
+        {
+            // GameObject.Find("OyuncuBekleniyor").GetComponent<TextMeshProUGUI>().text= "8888Oyuncu Bekleniyor";
+            GameObject.FindWithTag("OyuncuBekleniyor").GetComponent<TextMeshProUGUI>().text = "Oyuncu Bekleniyor";
+            GameObject.FindWithTag("Oyuncu_1").GetComponent<TextMeshProUGUI>().text = PhotonNetwork.PlayerList[0].NickName;
+            GameObject.FindWithTag("Oyuncu_2").GetComponent<TextMeshProUGUI>().text = ".....";
+            
+        }
+
+        
+    }
+    // Update is called once per frame
+    
+    public void SkorlariGuncelle(int oyuncusirasi,int sayiNedir)
+    {
+        
+        switch (oyuncusirasi)
+        {
+            case 0:
+                GameObject.FindWithTag("Oyuncu_1_skor").GetComponent<TextMeshProUGUI>().text = sayiNedir.ToString();
+            break;
+
+            case 1:
+                GameObject.FindWithTag("Oyuncu_2_skor").GetComponent<TextMeshProUGUI>().text =sayiNedir.ToString();
+            break;
+
+        }
+
+        if (sayiNedir <= 0)
+        {
+            if (oyuncusirasi==0)
+            {
+                foreach (GameObject objem in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
+                {
+                    if (objem.gameObject.CompareTag("KazanmaPaneli"))
+                    {
+                        objem.gameObject.SetActive(true);
+                        GameObject.FindWithTag("KazananKisi").GetComponent<TextMeshProUGUI>().text = "2.Oyuncu Kazandı";
+                    }
+                }
+
+            }
+            else
+            {
+                foreach (GameObject objem in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
+                {
+                    if (objem.gameObject.CompareTag("KazanmaPaneli"))
+                    {
+                        objem.gameObject.SetActive(true);
+                        GameObject.FindWithTag("KazananKisi").GetComponent<TextMeshProUGUI>().text = "1.Oyuncu Kazandı";
+                    }
+                }
+            }
+
+
+        }
+
+    }
+
+
+  
+}
